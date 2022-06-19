c61f289065fb48c1b709f6761f413549b649bb1a
Hadi Nazaripoor <hadi.nazaripoor@yahoo.com>
2022-06-19 20:53:08 +0430

    Add project files.

---
+++ SocialNetwork/Controllers/PostController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PostController : ControllerBase
+    {
+        //public IEnumerable<Post> Get()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ SocialNetwork/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Models
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Text { get; set; }
+        public bool Reported { get; set; }
+        public string UserID { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
